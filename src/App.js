cddd621d5b9f1f4fc9a5d5ec55e11cd633a5e1e7
your_username <kajantek@gmail.com>
2020-12-09 22:14:56 +0100

    Introducing - Routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import {Route, Switch} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import './App.css';
+
+const HatsPage = () => (
+  <div><h1>HATS PAGE</h1></div>
+);
+
 
 function App() {
   return (
     <div>
-      <HomePage />    
+      <Route exact={true} path='/' component={Homepage} />   
+      <Route exact={true} path='/hats' component={HatsPage} />  
     </div>
   );
 }
