038a1062f53f8546e89a42454bf729df3f7e8439
your_username <kajantek@gmail.com>
2020-12-13 01:00:09 +0100

    Images added + shop page + logo and header

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
-import HomePage from './pages/homepage/homepage.component.jsx';
+import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component.jsx';
+import Header from './components/header/header.component.jsx';
 
+// header called outside the switch so it can appear on all pages regardless of routing
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -5,25 +5,21 @@ import './menu-item.styles.scss';
 
 //lo style permette di aggiungere del css direttamente nel component
 const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
-
-     <div
-         className={`${size} menu-item`}
-         onClick={() => history.push(`${match.url}${linkUrl}`)}
-     >
-
-      <div
-        className='background-image' // background
-        style={{
-          backgroundImage: `url(${imageUrl})`
-        }}
-      />
-      
-      <div className='content' // testo 
-      >
-        <h1 className='title'>{title.toUpperCase()}</h1>
-        <span className='subtitle'>SHOP NOW</span>
-      </div>
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    <div
+      className='background-image'
+      style={{
+        backgroundImage: `url(${imageUrl})`
+      }}
+    />
+    <div className='content'>
+      <h1 className='title'>{title.toUpperCase()}</h1>
+      <span className='subtitle'>SHOP NOW</span>
     </div>
-  );
+  </div>
+);
 // withRouter è una funzione che ci permette di poternziare un componente et di fare un retourn per ritrovarcelo potenziato
 export default withRouter(MenuItem);
