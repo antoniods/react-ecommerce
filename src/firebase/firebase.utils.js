0169c68afcd89db938d234d66c8624398d441da9
your_username <kajantek@gmail.com>
2020-12-15 00:02:38 +0100

    added firebase connection

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,30 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+    apiKey: "AIzaSyBFa2nm3KeQkGOKwjNLBJVg2fiI29OCIAQ",
+    authDomain: "crwn-db-5407c.firebaseapp.com",
+    projectId: "crwn-db-5407c",
+    storageBucket: "crwn-db-5407c.appspot.com",
+    messagingSenderId: "528057157384",
+    appId: "1:528057157384:web:b66e1b9cdad491e8a68ebd"
+  };
+
+// Initialize Firebase
+firebase.initializeApp(config);
+
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+//access to GoogleAuthProvider Class to the auth library, do not forget the 'new'
+const provider = new firebase.auth.GoogleAuthProvider();
+
+//it popups googleauthprovider 
+provider.setCustomParameters({prompt : 'select_account'});
+
+//it takes the google signin only (there are a lot of types as twitter, linkedin...)
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
