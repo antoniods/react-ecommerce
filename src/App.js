07d4b467ed50cade2b213658428c0f7a8bbdce32
your_username <kajantek@gmail.com>
2020-12-16 22:41:18 +0100

    firebase settings .auth( )

--- src/App.js
+++ src/App.js
@@ -7,9 +7,31 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils.js';
 
 // header called outside the switch so it can appear on all pages regardless of routing
-function App() {
+// function App() { for having acces to state i need a class component
+class App extends React.Component{
+  constructor() {
+    super();
+    this.state = {
+      currentUser : null 
+    };
+
+  }
+
+  unsubrscibedFromApp = null
+
+  componentDidMount (){
+    this.unsubrscibedFromApp = auth.onAuthStateChanged(user=> {
+      this.setState({currentUser : user});
+
+      console.log(user);
+    });
+  }
+
+
+  render(){
   return (
     <div>
       <Header />
@@ -20,6 +42,7 @@ function App() {
       </Switch>
     </div>
   );
+}
 }
 
 export default App;
