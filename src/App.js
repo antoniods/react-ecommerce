584396543261cdfaed38cdfdcf935d376954966a
your_username <kajantek@gmail.com>
2020-12-16 23:59:43 +0100

    created sign-in page and sign-in component

--- src/App.js
+++ src/App.js
@@ -5,44 +5,47 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import Header from './components/header/header.component';
-import SignInSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils.js';
+import { auth } from './firebase/firebase.utils';
 
 // header called outside the switch so it can appear on all pages regardless of routing
 // function App() { for having acces to state i need a class component
-class App extends React.Component{
+class App extends React.Component {
   constructor() {
     super();
+
     this.state = {
-      currentUser : null 
+      currentUser: null
     };
-
   }
+  // how our app handle with firebase
+  unsubscribeFromAuth = null;
 
-  unsubrscibedFromApp = null
-
-  componentDidMount (){
-    this.unsubrscibedFromApp = auth.onAuthStateChanged(user=> {
-      this.setState({currentUser : user});
+  componentDidMount() {
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({ currentUser: user });
 
       console.log(user);
     });
   }
 
+  componentWillUnmount() {
+    this.unsubscribeFromAuth();
+  }
 
-  render(){
-  return (
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-        <Route path='/sign-in' component={SignInSignOutPage} />
-      </Switch>
-    </div>
-  );
-}
+  render() {
+    return (
+      <div>
+        <Header currentUser={this.state.currentUser} />
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { signInWithGoogle } from '../../firebase/firebase.utils.js';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 import './sign-in.styles.scss';
 
@@ -51,8 +52,12 @@ class SignIn extends React.Component {
             label='password'
             required
           />
-          <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={signInWithGoogle}>{' '}Sign in with Google{' '}</CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'> Sign in </CustomButton>
+            <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+              Sign in with Google
+            </CustomButton>
+          </div>
         </form>
       </div>
     );
